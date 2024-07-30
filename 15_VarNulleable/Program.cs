a44d664b897b9f8dbf1a0e64420f4d112003ec04
jotalexvalencia <jorgealexandervalencia@gmail.com>
2024-07-30 12:36:51 -0500

    15_VarNullable

---
+++ 15_VarNulleable/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _15_VarNulleable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Decalaración ímplicita de variables
+            // Solo se permiten en variables locales
+            // No se pueden usar en atributos
+            // No se pueden usar como tipos de retorno
+            // Se debe de colocar un valor en el momento de declaración y no puede ser null
+            // No confundir con var o Variant de otros lenguajes, es fuertemente tipificado
+            // Se usa en LINQ donde un query puede dar un resultado dinamicamente creado
+
+
+        }
+    }
+}
